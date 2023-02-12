31cf2a45d154c579dab4e98a040781ba1ae5f128
Alessandra Medeiros Vieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-02-12 20:29:13 -0300

    add arquivos

---
+++ EXERCICIO2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write ("Informe a base do Triângulo: ");
+
+int baseTri = int.Parse (Console.ReadLine());  // intrução irá aguardar a digitação do usuário.
+
+Console.Write ("Informe a altura do Triângulo: ");
+
+int alturaTri = int. Parse (Console.ReadLine());  // intrução irá aguardar a digitação do usuário.
+
+int areaResultado = ((baseTri * alturaTri)/2);
+
+Console.WriteLine ("A área do Triângulo é " + areaResultado);
+
+
+
+
+
+
+
+
+
+
+
