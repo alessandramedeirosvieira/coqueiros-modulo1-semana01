3098ed338588c63c2b2406ac5da36c365064e6d0
Alessandra Medeiros Vieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-02-12 20:58:44 -0300

    add arquivos

---
+++ EXERCICIO7A/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write ("Informe a 1ª nota:");
+double nota1 = double.Parse (Console.ReadLine ());
+
+Console.Write (" Informe a 2ª nota:");
+double nota2 = double.Parse (Console.ReadLine ());
+
+Console.Write (" Informe a 3ª nota:");
+double nota3 = double.Parse (Console.ReadLine ());
+
+double mediaNota = ((nota1 + nota2 + nota3)/3);
+
+if (mediaNota >=6) {}
+else if (mediaNota >=5) {}
+else {}
+
+string [] nomes;
+Double [] nota = new Double [4] {0,1,2,3};
+
